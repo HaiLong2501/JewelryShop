eb3f8859134065d6b4f1aba58e87fafe5c1d1e58
Hai Long <longx3732@gmail.com>
2025-05-17 17:22:37 +0700

    fifth committed

--- PNJShop/src/java/dal/DBContext.java
+++ PNJShop/src/java/dal/DBContext.java
@@ -29,7 +29,7 @@ public class DBContext {
         try {
             String user = "sa";
             String pass = "123";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=assignment_PRJ_SP25";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
